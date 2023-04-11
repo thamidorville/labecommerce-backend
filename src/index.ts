28a40da112c74163372a332ecac7f24aa58e4877
thamidorville <thamisdorville@gmail.com>
2023-04-11 14:38:57 -0300

    exe-knex-resolvidos

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { users, products, purchase, createUser, getAllUsers, getAllProducts, cre
 import { ROUPASESPACIAIS, TProduct, TPurchase, TUser } from "./types";
 import express, {Request, Response} from 'express'
 import cors from 'cors'
+import { db } from "./database/knex";
 
 console.log("TUsers:", users);
 console.log("TProducts:", products)
@@ -42,17 +43,30 @@ app.get('/ping', (req:Request, res:Response) => {
 //APIS e express exercicio 2. crie endpoints para a manipulacao dos dados do arquivo database.ts
 
 //getAllUsers
-app.get('/users', (req:Request, res:Response) => {
+
+
+                    // INTRO-KNEX - EXERCÍCIO 1
+                    // REFATORE OS SEGUINTES ENDPOINTS
+                    //GET ALL USERS
+
+
+app.get('/users', async (req:Request, res:Response) => {
     try {
+
+        const result = await db.raw(`SELECT * FROM users`)
+
         res.status(200).send(users)
     } catch (error) {
         res.status(500).send("erro ao carregar os usuários.")
     }
 })
-
+                            // INTRO-KNEX - EXERCÍCIO 1
+                            // REFATORE OS SEGUINTES ENDPOINTS
+                            //GET ALL PRODUCTS
 //getAllProducts
-app.get('/products', (req:Request, res:Response) => {
+app.get('/products', async (req:Request, res:Response) => {
     try {
+        const result = await db.raw(`SELECT * FROM products`)
         res.status(200).send(products)
     } catch (error) {
         res.status(500).send("erro ao carregar os produtos.")
@@ -63,17 +77,25 @@ app.get('/products', (req:Request, res:Response) => {
 //exercicio 1 - fluxo de dados em back end
 //query params deve possuir pelo menos 1 caractere
 
-app.get('/products/search', (req:Request, res:Response)=>{
+
+                            // INTRO-KNEX - EXERCÍCIO 1
+                            // REFATORE OS SEGUINTES ENDPOINTS
+                            //SEARCH PRODUCT BY NAME
+
+app.get('/products/search', async (req:Request, res:Response)=>{
   try {
     const q = req.query.q as string
 
     if(!q || q.length === 0){
         throw new Error("parâmetro deve possuir pelo menos um caractere")
     }
-    
-    const productsFilter = products.filter((product) => 
-    product.name.toLowerCase().includes(q.toLowerCase())
-    )
+
+    // const productsFilter = products.filter((product) => 
+    // product.name.toLowerCase().includes(q.toLowerCase())
+    // )
+
+        const productsFilter = await db.raw(`SELECT * FROM products WHERE (name) LIKE '%${q.toLowerCase()}%'`);
+
     res.status(200).send(productsFilter)
   } catch (error) {
     if(error instanceof Error){ 
@@ -90,84 +112,167 @@ app.get('/products/search', (req:Request, res:Response)=>{
 //nao deve ser possivel criar mais de uma conta com a mesma id
 //nao deve ser possivel criar mais de uma conta com o mesmo email
 
-app.post('/users', (req:Request, res:Response) => {
- try {
-    const id = req.body.id
-    const name = req.body.name
-    const email = req.body.email
-    const password = req.body.password
+// app.post('/users', (req:Request, res:Response) => {
+//  try {
+//     const id = req.body.id
+//     const name = req.body.name
+//     const email = req.body.email
+//     const password = req.body.password
 
-    const newUser: TUser = {
-        id, 
-        name,
-        email, 
-        password
-    }
-
-    if(!id || !name || !email || !password){
-        throw new Error("Preenchimento de todos os campos são obrigatórios.")
-    }
-
-    const verificarUsuarioComMesmoId = users.find((user) => user.id === id)
-    if(verificarUsuarioComMesmoId){
-        throw new Error("Já existe um usuário com este id.")
-    }
-    const emailRepetido = users.find((user) => user.email === email)
-    if(emailRepetido){
-        throw new Error("Já existe um usuário com este email.")
-    }
-
-    users.push(newUser)
-    res.status(201).send("Cadastro realizado com sucesso!")
+//     const newUser: TUser = {
+//         id, 
+//         name,
+//         email, 
+//         password
+//     }
 
 
-} catch (error) {
-    if (error instanceof Error) {
-        res.status(400).send(error.message);
-    } else {
-        console.error(error);
-        res.status(500).send("Erro interno do servidor");
-    }
- }
-})
+//     if(!id || !name || !email || !password){
+//         throw new Error("Preenchimento de todos os campos são obrigatórios.")
+//     }
+
+//     const verificarUsuarioComMesmoId = users.find((user) => user.id === id)
+//     if(verificarUsuarioComMesmoId){
+//         throw new Error("Já existe um usuário com este id.")
+//     }
+//     const emailRepetido = users.find((user) => user.email === email)
+//     if(emailRepetido){
+//         throw new Error("Já existe um usuário com este email.")
+//     }
+
+//     users.push(newUser)
+//     res.status(201).send("Cadastro realizado com sucesso!")
+
+
+// } catch (error) {
+//     if (error instanceof Error) {
+//         res.status(400).send(error.message);
+//     } else {
+//         console.error(error);
+//         res.status(500).send("Erro interno do servidor");
+//     }
+//  }
+// })
+
+
+                        //INTRO-KNEX EXERCÍCIO 2
+                    //Em seguida, refatore (ou recrie) os seguintes endpoints:
+
+// Create User
+// method HTTP (POST)
+// path ("/users")
+// body
+// id
+// name
+// email
+// password
+// createdAt
+// response
+// status 201
+// "Cadastro realizado com sucesso"
+
+        app.post("/create-table-users", async (req:Request, res:Response) => {
+            try {
+        
+            await db.raw(`
+            CREATE TABLE users (
+                id TEXT PRIMARY KEY NOT NULL,
+                name TEXT NOT NULL,
+                email TEXT NOT NULL UNIQUE,
+                password TEXT NOT NULL,
+                createdAt TEXT NOT NULL);
+            `)
+             res.status(200).send("Cadastro realizado com sucesso!")
+            } catch (error) {
+                if (error instanceof Error) {
+                    res.status(400).send(error.message);
+                } else {
+                    console.error(error);
+                    res.status(500).send("Erro interno do servidor");
+                }
+             }
+    
+    })
+
 
 //create product
 // exercicio 1 - fluxo de dados back end
 //validar o body
 // não deve ser possível criar mais de um produto com a mesma id
 
-app.post('/products', (req:Request, res:Response) => {
-    try {
-        const id = req.body.id
-    const name = req.body.name
-    const price = req.body.price
-    const category = req.body.category
+// app.post('/products', (req:Request, res:Response) => {
+//     try {
+//     const id = req.body.id
+//     const name = req.body.name
+//     const price = req.body.price
+//     const category = req.body.category
 
-    const newProduct: TProduct = {
-        id, 
-        name, 
-        price, 
-        category
-    }
+//     const newProduct: TProduct = {
+//         id, 
+//         name, 
+//         price, 
+//         category
+//     }
 
-    if (!id || !name || !price || !category){
-        throw new Error("Todos os campos devem ser preenchidos obrigatoriamente!")
-    }
-    const produtoComIdRepetida = products.find((product) => product.id === id)
-    if(produtoComIdRepetida){
-        throw new Error("Produto com id repetida.")
-    }
-    products.push(newProduct)
-    res.status(201).send("Produto cadastrado com sucesso!")
-    } catch (error) {
-        if(error instanceof Error){
-            res.status(400).send(error.message)
-        } else {
-            res.status(400).send("erro desconhecido.")
-        }
+//     if (!id || !name || !price || !category){
+//         throw new Error("Todos os campos devem ser preenchidos obrigatoriamente!")
+//     }
+//     const produtoComIdRepetida = products.find((product) => product.id === id)
+//     if(produtoComIdRepetida){
+//         throw new Error("Produto com id repetida.")
+//     }
+//     products.push(newProduct)
+//     res.status(201).send("Produto cadastrado com sucesso!")
+//     } catch (error) {
+//         if(error instanceof Error){
+//             res.status(400).send(error.message)
+//         } else {
+//             res.status(400).send("erro desconhecido.")
+//         }
         
-    }
-})
+//     }
+// })
+
+                        //INTRO-KNEX - EXERCÍCIO 2
+
+                        // Create Product
+                        // method HTTP (POST)
+                        // path ("/products")
+                        // body
+                        // id
+                        // name
+                        // price
+                        // description
+                        // imageUrl
+                        // response
+                        // status 201
+                        // "Produto cadastrado com sucesso"
+
+        app.post("/create-table/products", async (req:Request, res:Response) => {
+            
+            try {
+               await db.raw(`
+                CREATE TABLE products(
+                id TEXT PRIMARY KEY NOT NULL, 
+                name TEXT NOT NULL,
+                price REAL NOT NULL,
+                description TEXT NOT NULL,
+                imageUrl TEXT NOT NULL
+               `);
+                        res.status(201).send("Produto cadastrado com sucesso!")
+            } catch (error) {
+                if(error instanceof Error){
+                                res.status(400).send(error.message)
+                            } else {
+                                res.status(400).send("erro desconhecido.")
+                            }
+                            
+                        }
+
+        })
+
+
+
 //create purchase
 //exercicio 1 - fluxo de dados back end
 //validar o body 
@@ -175,36 +280,75 @@ app.post('/products', (req:Request, res:Response) => {
 // - id do produto que foi comprado deve existir no array de produtos cadastrados
 // - a quantidade e o total da compra devem estar com o cálculo correto
 
-app.post('/purchases', (req:Request, res:Response) => {
-    try {
-        const userId = req.body.id
-    const productId = req.body.id
-    const quantity = req.body.quantity
-    const totalPrice = req.body.totalPrice
 
-    const newPurchase: TPurchase = {
-        userId,
-        productId,
-        quantity,
-        totalPrice
-    }
 
-    const idProdutoComprado = products.find((product) => product.id === productId)
-    const usuarioComprador = users.find((user) => user.id === userId)
+                            //INTRO-KNEX EXERCÍCIO 2
+                            //CREATE PURCHASE
+//                             Create Purchase
+// method HTTP (POST)
 
-        if(!idProdutoComprado){
-            throw new Error("produto não encontrado.")
-        }
-        if(!usuarioComprador){
-            throw new Error("usuário não encontrado.")
-        }
-        if(!quantity || quantity < 1){
-            throw new Error("Quantidade inválida.")
-        }
-        const precoTotal = quantity * productId.price
+// path ("/purchases")
+
+// body
+
+// id
+// buyer
+// totalPrice
+// createdAt
+// paid
+// response
+
+// status 201
+// "Compra cadastrada com sucesso"
+            
+// app.post('/purchases', (req:Request, res:Response) => {
+//     try {
+//     const userId = req.body.id
+//     const productId = req.body.id
+//     const quantity = req.body.quantity
+//     const totalPrice = req.body.totalPrice
+
+//     const newPurchase: TPurchase = {
+//         userId,
+//         productId,
+//         quantity,
+//         totalPrice
+//     }
+
+//     const idProdutoComprado = products.find((product) => product.id === productId)
+//     const usuarioComprador = users.find((user) => user.id === userId)
+
+//         if(!idProdutoComprado){
+//             throw new Error("produto não encontrado.")
+//         }
+//         if(!usuarioComprador){
+//             throw new Error("usuário não encontrado.")
+//         }
+//         if(!quantity || quantity < 1){
+//             throw new Error("Quantidade inválida.")
+//         }
+//         const precoTotal = quantity * productId.price
     
-    purchase.push(newPurchase)
-    res.status(201).send("Compra realizada com sucesso!")
+//     purchase.push(newPurchase)
+
+
+app.post("/create-table-purchases", async (req:Request, res: Response)=>{
+    try {
+
+        await db.raw(`
+        
+            CREATE TABLE purchases(
+            id TEXT PRIMARY KEY NOT NULL,
+            buyer TEXT NOT NULL,
+            totalPrice REAL NOT NULL,
+            createdAt TEXT NOT NULL,
+            paid INTEGER NOT NULL,
+            FOREIGN KEY (buyer) REFERENCES users(id)
+        
+        `)
+                res.status(201).send("Compra cadastrada com sucesso!")
+
+        
     } catch (error) {
         if(error instanceof Error){
             res.status(400).send(error.message)
@@ -212,7 +356,12 @@ app.post('/purchases', (req:Request, res:Response) => {
             res.status(400).send("erro desconhecido.")
         }
     }
-})
+    })
+// })
+//     res.status(201).send("Compra cadastrada com sucesso!")
+//     } catch (error) {
+        
+// })
 
 //APROFUNDAMENTO EXPRESS 
 
@@ -223,16 +372,26 @@ app.post('/purchases', (req:Request, res:Response) => {
 // FLUXO DE DADOS BACK END EXERCICIO 2
 //VALIDAR QUE O PRODUTO EXISTE
 
-app.get('/products/:id', (req:Request, res:Response) => {
+
+                        //INTRO-KNEX EXERCÍCIO 3
+                        // GET PRODUCTS BY ID
+
+app.get('/products/:id', async (req:Request, res:Response) => {
 
    try {
     const id = req.params.id
-    const result = products.find((product) => product.id === id)
+    // const result = products.find((product) => product.id === id)
 
+        const result = await db.raw(`
+            SELECT * FROM products
+            WHERE id = "${id}"
+
+        `);
     if(!result){
         throw new Error("Produto não encontrado.")
     }
-    res.status(200).send(`Produto encontrado!', ${JSON.stringify(result)}`)
+    // res.status(200).send(`Produto encontrado!', ${JSON.stringify(result)}`)
+    res.status(200).send("objeto encontrado do arquivo .db")
     
    } catch (error) {
     if(error instanceof Error){
@@ -247,20 +406,35 @@ app.get('/products/:id', (req:Request, res:Response) => {
 // exercicio 2 - fluxo de dados back end
 //validade que o usuario existe
 
-app.get('/users/:id/purchases', (req:Request, res:Response) => {
+
+                        //INTRO-KNEX EXERCÍCIO 3
+                        //GET USER PURCHASES BY USER ID
+
+                        // method HTTP (GET)
+                        // path ("/users/:id/purchases")
+                        // response
+                        // status 200
+                        // array de compras do user no arquivo .db
+
+app.get('/users/:id/purchases', async (req:Request, res:Response) => {
 try {
     const userId = req.params.id
 
-const userPurchase = purchase.find((purchase) => purchase.userId === userId )
-
+// const userPurchase = purchase.find((purchase) => purchase.userId === userId )
+                const userPurchase = await db.raw(`
+                SELECT * FROM purchases
+                WHERE userId = "${userId}"
+                
+                `);
     if(!userPurchase){
         throw new Error("Id do usuario comprador nao identificada.")
 
     }
 
-    res.status(200).send(`id de Compra encontrada: ${JSON.stringify(userPurchase)}`)
+    // res.status(200).send(`id de Compra encontrada: ${JSON.stringify(userPurchase)}`)
 //     userPurchase ? res.status(200).send(`id de Compra encontrada: ${JSON.stringify(userPurchase)}`) 
 // : res.status(400).send('id de Compra não encontrada!')
+            res.status(200).send("array de compras do user no arquivo .db")
 } catch (error) {
     if(error instanceof Error){
         res.status(400).send('id de Compra não encontrada!')
